a569c9c45a73b49bb577e020c95484d28c09893a
Prince Kumar Sharma <prince.sharma@innofied.com>
2025-03-05 15:16:21 +0530

    Tab Menu UI Done

--- lib/presentation/screens/map_view_screen.dart
+++ lib/presentation/screens/map_view_screen.dart
@@ -1,49 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:task_management/presentation/widgets/card_widgets.dart';
+import 'package:task_management/presentation/widgets/map_widgets.dart';
 
-class MapViewScreen extends StatefulWidget {
+class MapViewScreen extends StatelessWidget {
   const MapViewScreen({super.key});
 
   @override
-  State<MapViewScreen> createState() => _MapViewScreenState();
-}
-
-class _MapViewScreenState extends State<MapViewScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Tasks'),
-      ),
-        drawer: Drawer(
-      child: ListView(
-      padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            child: Text(
-              'Task Management',
-              style: TextStyle(color: Colors.black, fontSize: 24),
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Map View'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.list),
-            title: Text('List View'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
+    double fontSize = MediaQuery.sizeOf(context).height * 0.022;
 
-        ],
+    return DefaultTabController(
+      length: 2, // Two tabs: Map View & List View
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text(
+            'Tasks',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          bottom: TabBar(
+            labelColor: Colors.black,
+            // Active tab text color
+            unselectedLabelColor: Colors.grey,
+            // Inactive tab text color
+            labelStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold, // Bold for active tab
+            ),
+            indicator: UnderlineTabIndicator(
+              borderSide: BorderSide(
+                width: 4,
+                color: Colors.black,
+                strokeAlign: BorderSide.strokeAlignOutside,
+              ),
+              insets: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * -0.08,
+              ),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            tabs: const [
+              Tab(text: "Map View"),
+              Tab(text: "List View"),
+            ],
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                child: Text(
+                  'Task Management',
+                  style: TextStyle(color: Colors.black, fontSize: 24),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.map),
+                title: const Text('Map View'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.list),
+                title: const Text('List View'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            MapScreen(), // Your map widget
+            CardWidgets()
+          ],
+        ),
       ),
-    ),
-      );
+    );
   }
 }
-
---
+++ lib/presentation/widgets/map_widgets.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200, // Set height for the container
+      color: Colors.blue,
+    );
+  }
+}
