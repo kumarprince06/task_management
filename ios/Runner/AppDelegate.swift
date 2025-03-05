409a7b1a2bc4b46fd99f14da5cd337cbb6715aa2
innofied <shreya.singh@innofied.com>
2025-03-05 18:23:17 +0530

    map added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyBYosiNXoeAD3Odftz6BaYchMO_MZ0ZMVo")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/presentation/widgets/map_widgets.dart
+++ lib/presentation/widgets/map_widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -8,11 +9,30 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _initialPosition = LatLng(37.7749, -122.4194); // San Francisco
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200, // Set height for the container
-      color: Colors.blue,
+    return SizedBox(
+      height: 400, // Set height for the container
+      child: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: 12.0, // Valid zoom level (between 0 and 21)
+        ),
+        zoomControlsEnabled: false, // Hide zoom buttons
+        zoomGesturesEnabled: true,  // Enable pinch-to-zoom
+        scrollGesturesEnabled: true, // Allow map scrolling
+        myLocationEnabled: true, // Enable location button
+        myLocationButtonEnabled: false, // Enable pinch-to-zoom
+      ),
     );
   }
 }
